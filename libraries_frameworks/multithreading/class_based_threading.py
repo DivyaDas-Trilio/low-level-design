7a12a822218ba192e994582254e4ff6aad8643d4
Divya Das <divya.das@trilio.io>
2025-08-26 09:05:08 +0530

    Adding threading concepts.

--- libraries_frameworks/multithreading/class_based_threading.py
+++ libraries_frameworks/multithreading/class_based_threading.py
@@ -1,4 +1,6 @@
+from concurrent.futures import thread
 from threading import Thread
+import threading
 
 class MyThread(Thread):
     def __init__(self):
@@ -11,5 +13,11 @@ class MyThread(Thread):
 
 if __name__ == '__main__':
     t1 = MyThread()
+    t2 = MyThread()
+    t3 = MyThread()
     t1.start()
-    t1.join()
+    t2.start()
+    t3.start()
+    l = threading.enumerate()
+    for each in l:
+        print(each.name)
