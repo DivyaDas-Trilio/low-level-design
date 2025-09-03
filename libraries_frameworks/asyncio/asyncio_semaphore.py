cf8e9e091a228575e342b2158b9a148e1db66c9c
Divya Das <divya.das@trilio.io>
2025-09-03 08:02:32 +0530

    Semaphore concept in asyncio.

---
+++ libraries_frameworks/asyncio/asyncio_semaphore.py
@@ -0,0 +1,21 @@
+import asyncio
+
+#lock = asyncio.Lock()
+lock = asyncio.Semaphore(2)
+
+async def print_name(name):
+    await lock.acquire()
+    for i in range(10):
+        print(f"Name:- {name}")
+        await asyncio.sleep(1)
+    lock.release()
+        
+
+async def main():
+    await asyncio.gather(
+        print_name('DHONI'),
+        print_name('KOHLI'),
+        print_name('SACHIN'))
+    
+if __name__ == '__main__':
+    asyncio.run(main())
