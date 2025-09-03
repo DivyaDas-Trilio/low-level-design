a36bdfd7e7525f17c8ee1277fbd53dc880171201
Divya Das <divya.das@trilio.io>
2025-09-03 07:51:34 +0530

    Resolving syncronization issues in asyncio.

--- libraries_frameworks/asyncio/synchronization_issue_with asyncio.py
+++ libraries_frameworks/asyncio/synchronization_issue_with asyncio.py
@@ -1,22 +1,27 @@
 import asyncio
 
 counter = 0
+async_lock = asyncio.Lock()
 
 async def increment():
+    await async_lock.acquire()
     global counter
     while(counter<10):
         counter += 1
         await asyncio.sleep(1)
         print(f"Counter:- {counter}") 
+    async_lock.release()
         
 async def decrement():
+    await async_lock.acquire()
     global counter
     #count = 0
     while(counter < 10):
         #count += 1
         counter -= 1
         await asyncio.sleep(0.75)
-        #print(f"Counter:- {counter}") 
+        #print(f"Counter:- {counter}")
+    async_lock.release() 
         
 
 async def main():
