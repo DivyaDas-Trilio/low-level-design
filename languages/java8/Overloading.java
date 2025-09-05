713d905a78380201166dde411621c5a1a8152f61
Divya Das <divya.das@trilio.io>
2025-09-05 10:16:11 +0530

    Method signature concept wrt Overloading.

--- languages/java8/Overloading.java
+++ languages/java8/Overloading.java
@@ -1,11 +1,32 @@
 public class Overloading{
+    // public void m1(){
+    //     System.out.println("No-args m1");
+    // }
+
+    // public int m1(int i){
+    //     System.out.println("int args m1");
+    //     return i;
+    // }
+
+    private String str1;
+    private int i;
+
     public void m1(){
-        System.out.println("No-args m1");
+        System.out.println("Parent m1 method");
     }
 
-    public int m1(int i){
-        System.out.println("int args m1");
-        return i;
+    public int m2(){
+        System.out.println("Parent m2 method.");
+        return 1;
+    }
+
+    public void m1(int i){
+        System.out.println("m1 int...");
+    }
+
+    public int m1(float f){
+        System.out.println("m1 float method...");
+        return 1;
     }
 }
 
@@ -13,6 +34,9 @@ class Test{
     public static void main(String[] args) {
         Overloading o = new Overloading();
         o.m1();
+        o.m2();
+
         o.m1(10);
+        o.m1(10.0f);
     }
 }
