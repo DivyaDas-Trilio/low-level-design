463fa865940e3075b0480b613ac089d5661978f3
Divya Das <divya.das@trilio.io>
2025-09-08 07:24:59 +0530

    Adding practise problems.

---
+++ libraries_frameworks/dataclasses/basic_dataclass.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self, name, age):
+        self.name: int = name 
+        self.age: int = age
+        
+    def __repr__(self):
+        return "Person"
+        
+
+class Employee(Person):
+    def __init__(self, name, age, emp_id, dept):
+        super().__init__(name, age)
+        self.emp_id: str = emp_id
+        self.dept: str = dept
+        
+    def __repr__(self):
+        return "Child"+super().__repr__()
+        
+        
+emp = Employee("dj", 31, 123, 'cse')
+print(emp)
