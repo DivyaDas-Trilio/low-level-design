6ef2574b0e51700880c7a9e359dcf56f146bac5f
Divya Das <divya.das@trilio.io>
2025-09-09 07:30:28 +0530

    ABCMeta, ABC, abstractclass in python3.

--- libraries_frameworks/abstract_base_class/abc_basics.py
+++ libraries_frameworks/abstract_base_class/abc_basics.py
@@ -1,7 +1,15 @@
 from abc import ABCMeta, abstractmethod, ABC
 
 class Animal(ABC):
-    pass
+    
+    @abstractmethod
+    def sound(self): pass
+    
+    @abstractmethod
+    def walk(self): pass
+    
+    def list_features(self):
+        print("List Features...l")
 
 
 if __name__ == "__main__":
