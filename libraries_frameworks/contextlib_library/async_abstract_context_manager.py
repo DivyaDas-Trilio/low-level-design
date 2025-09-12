5355753c2816b2bf110a833deda44af35c7b181d
Divya Das <divya.das@trilio.io>
2025-09-12 14:52:46 +0530

    async abstract context manager.

--- libraries_frameworks/contextlib_library/async_abstract_context_manager.py
+++ libraries_frameworks/contextlib_library/async_abstract_context_manager.py
@@ -1,19 +1,37 @@
 import asyncio
 
-class MyAsyncAbstractContextManager:
-    def __init__(self):
-        print("Init...")
+# class MyAsyncAbstractContextManager:
+#     def __init__(self):
+#         print("Init...")
         
-    async def __aenter__(self):
-        print("aenter ...")
-        return self
+#     async def __aenter__(self):
+#         print("aenter ...")
+#         return self
     
-    async def __aexit__(self, exc_type, exc_val, traceback):
-        print("aexit...")
+#     async def __aexit__(self, exc_type, exc_val, traceback):
+#         print("aexit...")
         
-async def main():
-      async with MyAsyncAbstractContextManager() as acntx:
-        print('inside async context manager.') 
+# async def main():
+#       async with MyAsyncAbstractContextManager() as acntx:
+#         print('inside async context manager.') 
 
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+
+from contextlib import AbstractAsyncContextManager
+
+class MyAsyncContextManager(AbstractAsyncContextManager):
+    def __init__(self):
+        print('init...')
+        
+    async def __aexit__(self, exc_type, exc_val, traceback):
+        print('aexit...')
+        
+
+async def main():
+    async with MyAsyncContextManager() as acntx:
+        print('inside async context manager...')
+        
 if __name__ == "__main__":
     asyncio.run(main())
